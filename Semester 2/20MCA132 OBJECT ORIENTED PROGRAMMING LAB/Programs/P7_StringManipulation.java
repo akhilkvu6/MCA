d1510f0210573c3ef70e6f5ce9fb86bf61a7cd21
Akhil Krishna <akhilkvu6@gmail.com>
2026-05-08 14:56:01 +0530

    Added Program 7 - String Manipulation with detailed README

---
+++ Semester 2/20MCA132 OBJECT ORIENTED PROGRAMMING LAB/Programs/P7_StringManipulation.java
@@ -0,0 +1,35 @@
+// Program 7: Perform string manipulation using Java string functions.
+
+import java.util.Scanner;
+
+public class P7_StringManipulation
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        System.out.print("Enter another string: ");
+        String str2 = sc.nextLine();
+
+        System.out.println("\nLowercase version: " + s.toLowerCase());
+
+        System.out.println("Uppercase version: " + s.toUpperCase());
+
+        System.out.println("Length of string: " + s.length());
+
+        System.out.println("Substring starting from index 2: " + s.substring(2));
+
+        System.out.println("Substring from index 1 to 4: " + s.substring(1, 4));
+
+        System.out.println("Trimmed string: " + s.trim());
+
+        System.out.println("Index of first occurrence of 'a': " + s.indexOf('a'));
+
+        System.out.println("Index of 'a' starting from index 2: " + s.indexOf('a', 2));
+
+        System.out.println("Concatenated string: " + s.concat(str2));
+    }
+}
